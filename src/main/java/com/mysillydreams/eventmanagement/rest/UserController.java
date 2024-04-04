47f4ebdda3498ad127d93ec18c4d2ca333462ec3
vijayyk7676 <vijayyk7676@gmail.com>
2024-04-04 11:20:24 +0530

    deploy demo2

--- src/main/java/com/mysillydreams/eventmanagement/rest/UserController.java
+++ src/main/java/com/mysillydreams/eventmanagement/rest/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "Hi, MySillyDreams!!!!!";
+    public String hello() {
+        return "<h1>Welcome to MySillyDreams</h1>" +
+                "<h2>A place where dreams come true!</h2>" +
+                "<a href='https://www.mysillydreams.com'>Visit MySillyDreams</a>";
     }
 
 
